503940978a385e4f124675328366c47628fdd97f
Cho Yong Jae <dydwo706@naver.com>
2022-02-27 18:26:57 +0900

    mola

--- RP/src/main/webapp/scrap.js
+++ RP/src/main/webapp/scrap.js
@@ -1,28 +1,36 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+function getGithubLink(id){
+    const axios = require('axios');
+    const cheerio = require('cheerio');
 
-let url = "https://github.com/search?q=hangman";
+    let url = "https://github.com/search?q=hangman";
 
-axios.get(url)
-    .then(html => {
-        const $ = cheerio.load(html.data);
 
-        var nameArr = [];
-        $('div.container-lg px-md-2 mt-lg-4 clearfix')
-            .find('div.col-12 col-md-9 float-left px-2 pt-3 pt-md-0 codesearch-results')
-            .find('ul.repo-list')
-            .find('li.repo-list-item hx_hit-repo d-flex flex-justify-start py-4 public source')
-            .find('div.mt-n1 flex-auto')
-            .find('div.d-flex')
-            .find('div.f4 text-normal')
-            .find('a')
-            .each((i, el) => {
-                nameArr.push(
-                    $(el).text()
-                        .replace(/\n/g, '')
-                        .replace(/ /g, '')
-                );
-            });
+    axios.get(url)
+        .then(html => {
+            const $ = cheerio.load(html.data);
 
-        console.log(nameArr);
-    })
+            var nameArr = [];
+            $('div.container-lg px-md-2 mt-lg-4 clearfix')
+                .find('div.col-12 col-md-9 float-left px-2 pt-3 pt-md-0 codesearch-results')
+                .find('ul.repo-list')
+                .find('li.repo-list-item hx_hit-repo d-flex flex-justify-start py-4 public source')
+                .find('div.mt-n1 flex-auto')
+                .find('div.d-flex')
+                .find('div.f4 text-normal')
+                .find('a')
+                .each((i, el) => {
+                    nameArr.push(
+                        $(el).text()
+                            .replace(/\n/g, '')
+                            .replace(/ /g, '')
+                    );
+                });
+            
+        })
+
+}
+
+function abc(id){
+    document.getElementById(id).innerText = '<p>시발</p>';
+    document.write("<p>tlqkftlqkf</p>");
+}
